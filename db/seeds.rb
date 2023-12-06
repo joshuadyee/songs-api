ecd66d1c7726671993b6605fd19a8fed0a899985
Joshua Yee <joshyee16@gmail.com>
2023-12-06 11:02:35 -0800

    create song instances in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+song = Song.new(
+  title: "Super Gremlin",
+  album: "Back For Everything",
+  artist: "Kodak Black",
+  year: "2022"
+)
+
+song.save
