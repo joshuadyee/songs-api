aa82984c09d0304254a14a0c4144707c237c8513
Joshua Yee <joshyee16@gmail.com>
2023-12-06 12:17:09 -0800

    add tests to controller test file

--- test/controllers/songs_controller_test.rb
+++ test/controllers/songs_controller_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 class SongsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/songs.json"
+    assert_response 200
+  
+    data = JSON.parse(response.body)
+    assert_equal Song.count, data.length
+  end
+  
+  test "show" do
+    get "/songs/#{Song.first.id}.json"
+    assert_response 200
+  
+    data = JSON.parse(response.body)
+    assert_equal ["id", "title", "album", "artist", "year", "created_at", "updated_at"], data.keys
+  end
+
+  test "create" do
+    assert_difference "Song.count", 1 do
+      post "/songs.json", params: { name: "test song", price: 1, image_url: "image.jpg", description: "test description"  }
+    end
+  end
+
+  test "update" do
+    song = Song.first
+    patch "/songs/#{song.id}.json", params: { title: "Updated title" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Updated title", data["title"]
+  end
+
+  test "destroy" do
+    assert_difference "Song.count", -1 do
+      delete "/songs/#{Song.first.id}.json"
+      assert_response 200
+    end
+  end
 end
