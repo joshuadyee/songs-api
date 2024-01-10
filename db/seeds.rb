7b90ebe72a06716dfbebae66caa4f6ef23269387
Joshua Yee <joshyee16@gmail.com>
2024-01-10 00:49:27 -0800

    add cors file and update gemfile, update seeds file with song classes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,44 @@
-song = Song.new(
-  title: "Super Gremlin",
-  album: "Back For Everything",
-  artist: "Kodak Black",
-  year: "2022"
-)
+# song = Song.new(
+#   title: "My Sacrifice",
+#   album: "Weathered",
+#   artist: "Creed",
+#   year: "2001"
+# )
 
-song.save
+# song.save
+
+# song = Song.new(
+#   title: "Higher",
+#   album: "Human Clay",
+#   artist: "Creed",
+#   year: "1999"
+# )
+
+# song.save
+
+# song = Song.new(
+#   title: "Higher Love",
+#   album: "Back In The High Life",
+#   artist: "Steve Winwood",
+#   year: "1986"
+# )
+
+# song.save
+
+# song = Song.new(
+#   title: "While You See A Chance",
+#   album: "Arc Of A Diver",
+#   artist: "Steve Winwood",
+#   year: "1980"
+# )
+
+# song.save
+
+# song = Song.new(
+#   title: "Juicy",
+#   album: "Ready To Die",
+#   artist: "The Notorious B.I.G.",
+#   year: "1994"
+# )
+
+# song.save
